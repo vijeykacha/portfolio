fcf30a9d37e64cc7e1686a7e5e2246490fe04440
Vijey <91133727+vijeykacha@users.noreply.github.com>
2023-07-29 00:44:28 +0530

    Hero image fixed

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
           <Image
-            src={hero}
+            src={hero.src}
             alt="Vijay Kacha"
             height={300}
             width={300}
